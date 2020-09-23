d6914364e2ebd76b12e020513296c58cfe1294f8
ft9738962 <lemonade475@163.com>
2020-09-24 00:36:16 +0800

    200924 4

--- 4.go
+++ 4.go
@@ -19,28 +19,25 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 		return getMedian(nums1)
 	}
 
-	low, hi, midSize, mid1 := 0, m-1, (m+n+1)>>1, m>>2
+	low, hi, midSize, mid1 := 0, m-1, (m+n+1)>>1, m>>1
 	mid2 := midSize - mid1
-	for nums1[mid1] > nums2[mid2+1] && nums1[mid1+1] < nums2[mid2] {
-		if nums1[mid1] > nums2[mid2] {
+	for nums1[mid1-1] > nums2[mid2] || nums1[mid1] < nums2[mid2-1] {
+		if nums1[mid1-1] > nums2[mid2] {
+			hi = hi >> 1
+		} else if nums1[mid1] < nums2[mid2-1] {
 			low = mid1
-
-		} else if nums1[mid1+1] < nums2[mid2] {
-			hi = mid1
 		}
-
 		mid1 = (low + hi + 1) >> 1
+		if mid1 == 0 {
+			return getMedian(append(nums2, nums1...))
+		} else if mid1 == hi {
+			return getMedian(append(nums1, nums2...))
+		}
 		mid2 = midSize - mid1
-
+		fmt.Println(mid1, mid2)
 	}
-
-	mi := math.Max(float64(nums1[mid1]), float64(nums2[mid2]))
-	if (n+m)%2 == 1 {
-		return mi
-	}
-	ma := math.Min(float64(nums1[mid1+1]), float64(nums2[mid2+1]))
-	return (mi + ma) / 2
-
+	return getMedian2(nums1[mid1-1], nums1[mid1],
+		nums2[mid2-1], nums2[mid2], n+m)
 }
 
 func getMedian(arr []int) float64 {
@@ -51,14 +48,18 @@ func getMedian(arr []int) float64 {
 	return float64(arr[length/2])
 }
 
-// func testFromMap(tests map[int][][]int, i int) {
-// 	a, b := tests[i]
-// 	return a, b
-// }
+func getMedian2(nums1Left, nums1Right, nums2Left, nums2Right, totLength int) float64 {
+	mi := math.Max(float64(nums1Left), float64(nums2Left))
+	if totLength%2 == 1 {
+		return mi
+	}
+	ma := math.Min(float64(nums1Right), float64(nums2Right))
+	return (mi + ma) / 2
+}
 
 func main() {
-	nums1 := []int{1, 3, 5, 7, 9}
-	nums2 := []int{2, 4, 6, 8}
+	nums1 := []int{1, 3, 5}
+	nums2 := []int{2, 4}
 	// nums3 := []int{0}
 	// nums5 := []int{0, 0}
 	// nums6 := []int{}
