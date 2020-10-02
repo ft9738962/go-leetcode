90db6957c0ebfc775900f6c58e35d96767f3672c
ft9738962 <lemonade475@163.com>
2020-10-03 07:56:12 +0800

    201009 11

--- code.go
+++ code.go
@@ -1,22 +1,27 @@
 package main
 
-func isMatch(s string, p string) bool {
-	m, n := len(s), len(p)
-	dp := make([][]bool, m+1)
-	for i := 0; i < m+1; i++ {
-		dp[i] = make([]bool, n+1)
-	}
-	dp[0][0] = true
-	for i := 0; i <= m; i++ {
-		for j := 1; j <= n; j++ {
-			if j > 1 && p[j-1] == '*' {
-				dp[i][j] = dp[i][j-2] || i > 0 && (s[i-1] == p[j-2] || p[j-2] == '.') && dp[i-1][j]
-			} else {
-				dp[i][j] = i > 0 && (s[i-1] == p[j-1] || p[j-1] == '.') && dp[i-1][j-1]
-			}
+func maxArea(height []int) int {
+	var max, curArea int
+	l, r := 0, len(height)-1
+	for l < r {
+		curArea = min(height[l], height[r]) * (r - l)
+		if curArea > max {
+			max = curArea
+		}
+		if height[l] < height[r] {
+			l++
+		} else {
+			r--
 		}
 	}
-	return dp[m][n]
+	return max
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 // func main() {
--- code_test.go
+++ code_test.go
@@ -4,72 +4,67 @@ import (
 	"testing"
 )
 
-func TestIsMatch(t *testing.T) {
-	test1S := "aa"
-	test1P := "a"
-	test2S := "aa"
-	test2P := "a*"
-	test3S := "ab"
-	test3P := ".*"
-	test4S := "aab"
-	test4P := "c*a*b"
-	test5S := "mississippi"
-	test5P := "mis*is*p*."
-	test6S := "aab"
-	test6P := "c*a**b"
-	test7S := "aaa"
-	test7P := "a*a"
-	test8S := "aaa"
-	test8P := "ab*a*c*a"
+func TestMaxArea(t *testing.T) {
+	test1 := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
+	test2 := []int{1, 1}
+	test3 := []int{4, 3, 2, 1, 4}
+	test4 := []int{1, 2, 1}
+	test5 := []int{2, 3, 4, 5, 18, 17, 6}
+	// test6S := "aab"
+	// test6P := "c*a**b"
+	// test7S := "aaa"
+	// test7P := "a*a"
+	// test8S := "aaa"
+	// test8P := "ab*a*c*a"
 	// 	str10 := "-   234"
 
-	got1 := isMatch(test1S, test1P)
-	want1 := false
+	got1 := maxArea(test1)
+	want1 := 49
 	if got1 != want1 {
-		t.Errorf("test 1 got %t want %t", got1, want1)
+		t.Errorf("test 1 got %d want %d", got1, want1)
 	}
 
-	got2 := isMatch(test2S, test2P)
-	want2 := true
+	got2 := maxArea(test2)
+	want2 := 1
 	if got2 != want2 {
-		t.Errorf("test 2 got %t want %t", got2, want2)
+		t.Errorf("test 2 got %d want %d", got2, want2)
 	}
 
-	got3 := isMatch(test3S, test3P)
-	want3 := true
+	got3 := maxArea(test3)
+	want3 := 16
 	if got3 != want3 {
-		t.Errorf("test 3 got %t want %t", got3, want3)
+		t.Errorf("test 3 got %d want %d", got3, want3)
 	}
 
-	got4 := isMatch(test4S, test4P)
-	want4 := true
+	got4 := maxArea(test4)
+	want4 := 2
 	if got4 != want4 {
-		t.Errorf("test 4 got %t want %t", got4, want4)
+		t.Errorf("test 4 got %d want %d", got4, want4)
 	}
 
-	got5 := isMatch(test5S, test5P)
-	want5 := false
+	got5 := maxArea(test5)
+	want5 := 17
 	if got5 != want5 {
-		t.Errorf("test 5 got %t want %t", got5, want5)
+		t.Errorf("test 5 got %d want %d", got5, want5)
 	}
 
-	got6 := isMatch(test6S, test6P)
-	want6 := false
-	if got6 != want6 {
-		t.Errorf("test 6 got %t want %t", got6, want6)
-	}
+	// got6 := isMatch(test6S, test6P)
+	// want6 := false
+	// if got6 != want6 {
+	// 	t.Errorf("test 6 got %t want %t", got6, want6)
+	// }
 
-	got7 := isMatch(test7S, test7P)
-	want7 := true
-	if got7 != want7 {
-		t.Errorf("test 7 got %t want %t", got7, want7)
-	}
+	// got7 := isMatch(test7S, test7P)
+	// want7 := true
+	// if got7 != want7 {
+	// 	t.Errorf("test 7 got %t want %t", got7, want7)
+	// }
 
-	got8 := isMatch(test8S, test8P)
-	want8 := true
-	if got8 != want8 {
-		t.Errorf("test 8 got %t want %t", got8, want8)
-	}
+	// got8 := isMatch(test8S, test8P)
+	// want8 := true
+	// if got8 != want8 {
+	// 	t.Errorf("test 8 got %t want %t", got8, want8)
+	// }
 
 	// got9 := myAtoi(str9)
 	// want9 := 2147483647
