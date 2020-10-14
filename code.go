ebf7aecf95dc401993d2fe6ec4ddcc68c751a354
ft9738962 <lemonade475@163.com>
2020-10-14 18:02:00 +0800

    201014 18

--- code.go
+++ code.go
@@ -1,9 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func fourSum(nums []int, target int) [][]int {
+	sort.Ints(nums)
+	var result = make([][]int, 0)
 
+	for i := range nums[:len(nums)-3] {
+		if i > 1 {
+			if nums[i] == nums[i-1] {
+				continue
+			}
+		}
+		rs := threeSum(nums[i+1:], target-nums[i])
+		for _, r := range rs {
+			var temp []int
+			result = append(result, append(append(temp, nums[i]), r...))
+		}
+	}
+	return result
+}
+
+func threeSum(nums []int, target int) [][]int {
+	if len(nums) < 3 {
+		return [][]int{}
+	}
+
+	sort.Ints(nums) // O(nlog(n))
+	var numsDict = make(map[int][]int)
+	var result [][]int
+
+	for i, n := range nums {
+		if _, ok := numsDict[n]; ok != true {
+			numsDict[n] = []int{i}
+		} else {
+			numsDict[n] = append(numsDict[n], i)
+		} // O(n)
+	}
+
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 {
+			if nums[i-1] == nums[i] {
+				continue
+			}
+			if nums[i-1]+nums[i] > 0 {
+				break
+			}
+		}
+
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j-1] == nums[j] && j > i+1 {
+				continue
+			}
+			if locs, ok := numsDict[target-nums[i]-nums[j]]; ok == true {
+				if locs[len(locs)-1] <= j {
+					continue
+				}
+				result = append(result,
+					[]int{nums[i], nums[j], target - nums[i] - nums[j]})
+			}
+			if nums[i]+nums[j]-target > 0 {
+				break
+			}
+		}
+	}
+	return result
 }
 
 // func abs(a int) int {
