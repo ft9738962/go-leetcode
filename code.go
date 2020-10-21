2ead5c12106e6ed8848dc2eda44e56632607b75d
ft9738962 <lemonade475@163.com>
2020-10-21 18:00:53 +0800

    201021 23

--- code.go
+++ code.go
@@ -2,41 +2,103 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 // ListNode ...
-// type ListNode struct {
-// 	Val  int
-// 	Next *ListNode
-// }
-
-func generateParenthesis(n int) []string {
-
-	result := []string{}
-	recursiveGenerate(&result, "", n, 0, 0, 0)
-	return result
+type ListNode struct {
+	Val  int
+	Next *ListNode
 }
 
-func recursiveGenerate(result *[]string, current string,
-	length, curLength, left, right int) {
-	if length == left && length == right {
-		*result = append(*result, current)
-	} else if left < length && left > right {
-		recursiveGenerate(result, current+"(",
-			length, curLength+1, left+1, right)
-		recursiveGenerate(result, current+")",
-			length, curLength+1, left, right+1)
-	} else if left == length {
-		recursiveGenerate(result, current+")",
-			length, curLength+1, left, right+1)
-	} else {
-		recursiveGenerate(result, current+"(",
-			length, curLength+1, left+1, right)
+func mergeKLists(lists []*ListNode) *ListNode {
+	var emp *ListNode
+	k := len(lists)
+	if k == 0 {
+		return emp
+	} else if k == 1 {
+		return lists[0]
 	}
+
+	order := []int{}
+	firstMap := map[int][]*ListNode{}
+	for i := 0; i < k; i++ {
+		if lists[i] != emp {
+			order = append(order, lists[i].Val)
+			if _, ok := firstMap[lists[i].Val]; ok != false {
+				firstMap[lists[i].Val] = append(firstMap[lists[i].Val], lists[i])
+			} else {
+				firstMap[lists[i].Val] = []*ListNode{lists[i]}
+			}
+		}
+	}
+	sort.Ints(order)
+	fmt.Println(len(order))
+	ct := 0
+	for j := 0; j < len(order); j++ {
+		if j > 0 {
+			if order[j] == order[j-1] {
+				continue
+			}
+		}
+		nodes := firstMap[order[j]]
+		for k := 0; k < len(nodes); k++ {
+			lists[ct] = nodes[k]
+			ct++
+			fmt.Println("ct:", ct)
+		}
+	}
+	lists = lists[:ct]
+
+	var resultHead, curResult *ListNode
+	resultHead = lists[0]
+	curResult = resultHead
+	for len(lists) > 1 {
+		if lists[0].Next == nil {
+			lists = lists[1:]
+			curResult.Next = lists[0]
+		}
+		for lists[0].Next != nil {
+			if lists[0].Next.Val <= lists[1].Val {
+				curResult = curResult.Next
+				lists[0] = lists[0].Next
+				continue
+			}
+			break
+		}
+		lists = reorderLists(lists)
+	}
+	curResult.Next = lists[0]
+	return resultHead
+}
+
+func reorderLists(lists []*ListNode) []*ListNode {
+	n := lists[0]
+	left, right, mid := 1, len(lists)-1, 0
+	for left < right {
+		mid = (left + right) / 2
+		if n.Val > lists[mid].Val {
+			left = mid + 1
+		} else if n.Val < lists[mid].Val {
+			right = mid
+		} else {
+			_ = copy(lists[1:mid], lists[0:mid-1])
+			lists[mid-1] = n
+			return lists
+		}
+	}
+	if lists[right].Val < n.Val {
+		_ = copy(lists[1:right+1], lists[:right])
+		lists[right] = n
+	} else {
+		_ = copy(lists[1:right], lists[:right-1])
+		lists[right-1] = n
+	}
+	return lists
 }
 
 func main() {
-	result := generateParenthesis(4)
-	fmt.Println(result)
+	// result := generateParenthesis(4)
+	// fmt.Println(result)
 
 }
