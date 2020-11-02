fa4c51e44e75a1fc5bc7c248724b23f161fe7716
ft9738962 <lemonade475@163.com>
2020-11-02 17:57:48 +0800

    201102 30

--- code.go
+++ code.go
@@ -8,45 +8,53 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func divide(dividend int, divisor int) int {
-	var sign, reducer, ct, result int
-	sign = 1
-
-	if dividend == 0 {
-		return 0
-	} else if divisor == 1 || divisor == -1 {
-		result = dividend * divisor
-	} else if dividend > 0 && divisor < 0 {
-		sign = -1
-		divisor = -1 * divisor
-	} else if dividend < 0 && divisor > 0 {
-		sign = -1
-		dividend = -1 * dividend
-	} else if dividend < 0 && divisor < 0 {
-		dividend = -1 * dividend
-		divisor = -1 * divisor
-	}
-
-	if result == 0 {
-		ct = 1
-
-		reducer = divisor
-		for dividend >= divisor {
-			if dividend < reducer {
-				reducer = divisor
-				ct = 1
+func findSubstring(s string, words []string) []int {
+	cws := &[]string{}
+	combineWords("", words, cws)
+	subStringDict := map[string]int{}
+	for _, word := range *cws {
+		if _, ok := subStringDict[word]; ok == true {
+			continue
+		}
+		subStringDict[word] = -1
+		l := len(word)
+		for i := 0; i < len(s)-l+1; i++ {
+			if string(s[i:i+l]) == word {
+				subStringDict[word] = i
+				break
 			}
-			dividend -= reducer
-			result += ct
-			reducer = reducer << 1
-			ct = ct << 1
 		}
 	}
-
-	if result > 1<<31-1 || result < -(1<<31) {
-		return (1<<31 - 1) * sign
+	result := []int{}
+	for _, ind := range subStringDict {
+		if ind > -1 {
+			result = append(result, ind)
+		}
 	}
-	return result * sign
+	return result
+}
+
+func combineWords(prefix string,
+	words []string, cws *[]string) {
+	if len(words) == 1 {
+		*cws = append(*cws, prefix+words[0])
+	} else {
+		for i := 0; i < len(words); i++ {
+			if i == 0 {
+				combineWords(prefix+string(words[i]),
+					words[i+1:],
+					cws)
+			} else if i == len(words)-1 {
+				combineWords(prefix+string(words[i]),
+					words[:i],
+					cws)
+			} else {
+				combineWords(prefix+string(words[i]),
+					append(words[:i], words[i+1:]...),
+					cws)
+			}
+		}
+	}
 }
 
 func printVals(head *ListNode) {
@@ -60,15 +68,17 @@ func printVals(head *ListNode) {
 
 func main() {
 	// test1 := []int{1, 1}
-	test2 := []int{7, -3}
+	// test2 := []int{7, -3}
 	// test3 := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
 	// a6 := ListNode{
 	// 	Val:  6,
 	// 	Next: nil,
 	// }
-	test5 := []int{-2147483648, 2}
+	// test5 := []int{-2147483648, 2}
 	// var c1 *ListNode
 
+	s := "barfoofoobarthefoobarman"
+	words := []string{"bar", "foo", "the"}
 	// d1 := ListNode{
 	// 	Val:  2,
 	// 	Next: nil,
@@ -109,10 +119,7 @@ func main() {
 	// fmt.Printf("answer1 %v\n", answer1)
 	// fmt.Printf("answer2 %v\n", answer2)
 	// fmt.Printf("answer3 %v\n", answer3)
-	r1 := divide(test2[0], test2[1])
-
-	r2 := divide(test5[0], test5[1])
-	fmt.Println(r1)
-	fmt.Println(r2)
+	fmt.Println(findSubstring(s, words))
+	// fmt.Println(r2)
 
 }
