fa7841be60a0e764296e246fc92a2cfff0eed238
ft9738962 <lemonade475@163.com>
2020-11-10 14:14:56 +0800

    201110 32

--- code.go
+++ code.go
@@ -3,84 +3,68 @@ package main
 import "fmt"
 
 // ListNode ...
-type ListNode struct {
-	Val  int
-	Next *ListNode
+type stack struct {
+	vals []int
+}
+
+func (s *stack) push(n int) {
+	s.vals = append(s.vals, n)
+}
+
+func (s *stack) pop() {
+	s.vals = s.vals[:len(s.vals)-1]
+}
+
+func (s *stack) insert(pos int, n int) {
+	if pos < 0 || pos > len(s.vals) {
+		fmt.Println("position is not valid")
+	}
+	if pos == 0 {
+		s.vals = append([]int{n}, s.vals...)
+	} else if pos == len(s.vals) {
+		s.push(n)
+	} else {
+		s.vals = append(append(s.vals[:pos], n), s.vals[pos+1:]...)
+	}
+}
+
+func (s *stack) peak() int {
+	return s.vals[len(s.vals)-1]
 }
 
 func longestValidParentheses(s string) int {
-	var longest []int
-	longest = []int{0}
-	var tmp int
-	for i := 1; i < len(s); i++ {
+	st := stack{[]int{}}
+	var maxLen int
+	for i := 0; i < len(s); i++ {
 		if string(s[i]) == "(" {
-			longest = append(longest, 0)
+			st.push(i)
 		} else {
-			if i-1 >= 0 && string(s[i-1]) == "(" {
-				if i == 1 {
-					longest = append(longest, 2)
+			if len(st.vals) > 0 {
+				if string(s[st.peak()]) == "(" {
+					st.pop()
 				} else {
-					longest = append(longest, longest[i-2]+2)
+					st.push(i)
 				}
-			} else if i-1 >= 0 && string(s[i-1]) == ")" && i-longest[i-1]-1 >= 0 && string(s[i-longest[i-1]-1]) == "(" {
-				if i-longest[i-1]-2 >= 0 {
-					tmp = longest[i-longest[i-1]-2]
-				} else {
-					tmp = 0
-				}
-				longest = append(longest, longest[i-1]+2+tmp)
 			} else {
-				longest = append(longest, 0)
+				st.push(i)
 			}
 		}
 	}
-	return maxLongest(longest)
-}
-
-func maxLongest(longest []int) int {
-	maxLen := 0
-	for i := 0; i < len(longest); i++ {
-		if longest[i] > maxLen {
-			maxLen = longest[i]
+	st.insert(0, -1)
+	st.push(len(s))
+	for i := 0; i < len(st.vals)-1; i++ {
+		if st.vals[i+1]-st.vals[i]-1 > maxLen {
+			maxLen = st.vals[i+1] - st.vals[i] - 1
 		}
 	}
 	return maxLen
 }
 
-// 	left := []int32{}
-// 	for _, r := range s {
-// 		if r == 40 {
-// 			left = append(left, r)
-// 		} else if len(left) >= 1 {
-// 			if r == 41 && left[len(left)-1] == 40 {
-// 				left = left[:len(left)-1]
-// 			} else {
-// 				return false
-// 			}
-// 		} else {
-// 			return false
-// 		}
-// 	}
-// 	if len(left) > 0 {
-// 		return false
-// 	}
-// 	return true
-// }
-
-// func printVals(head *ListNode) {
-// 	fmt.Println("start")
-// 	for head != nil {
-// 		fmt.Println(head.Val)
-// 		head = head.Next
-// 	}
-// 	fmt.Println("end")
-// }
-
 func main() {
 
 	// in := ")()())()()()(())))()()()()"
 	// in2 := "()(())"
-	in3 := "(()"
+	in3 := ")()())"
 	in4 := ")(((((()())()()))()(()))("
 	in5 := ")(()(()(((())(((((()()))((((()()(()()())())())()))()()()())(())()()(((()))))()((()))(((())()((()()())((())))(())))())((()())()()((()((())))))((()(((((()((()))(()()(())))((()))()))())"
 	// fmt.Println(longestValidParentheses(in))
