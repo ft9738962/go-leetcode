dd9f2b4c562e3c769c7481694bb15458c2b4fcec
ft9738962 <lemonade475@163.com>
2020-11-12 16:36:14 +0800

    201112 33

--- 33 Search in Rotated Sorted Array.md
+++ 33 Search in Rotated Sorted Array.md
@@ -29,3 +29,78 @@ Output: -1
 - All values of nums are unique.
 - nums is guranteed to be rotated at some pivot.
 - -10^4 <= target <= 10^4
+
+### 思路：
+使用二分法锁定目标值的可能范围进行迭代，因为数组不是单向递增递减，且最左侧值，必定大于最右侧值，其中值有两种情况：
+- 中值大于最左侧，从最左侧至中值为单向递增，中值至最右侧仍类似原来的情形
+- 中值小于最右侧，从中值至最右侧为单向递增，最左侧至中值仍类似原来的情形
+通过对目标值的迭代，找到目标值所属的单向递增区，然后使用普通的二分查找
+
+```go
+// runtime 0ms 100% memory 2.6MB 100%
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	result := -1
+	recurSearch(nums, target, l, r, &result)
+	return result
+}
+
+func recurSearch(nums []int, target int, l, r int, result *int) {
+	mid := (l + r) / 2
+	if mid == l {
+		if target == nums[l] {
+			*result = l
+		}
+		if target == nums[r] {
+			*result = r
+		}
+	} else {
+		if nums[mid] >= nums[l] {
+			if target >= nums[l] {
+				if nums[mid] >= target {
+					oneDirectionSearch(nums, target, l, mid, result)
+				} else {
+					recurSearch(nums, target, mid+1, r, result)
+				}
+			} else {
+				recurSearch(nums, target, mid+1, r, result)
+			}
+		} else {
+			if target >= nums[l] {
+				recurSearch(nums, target, l, mid, result)
+			} else {
+				if target <= nums[r] {
+					if target >= nums[mid] {
+						oneDirectionSearch(nums, target, mid, r, result)
+					} else {
+						recurSearch(nums, target, l, mid-1, result)
+					}
+				}
+			}
+		}
+	}
+}
+
+func oneDirectionSearch(nums []int, target int, l, r int, result *int) {
+	mid := (l + r) / 2
+	for mid != l {
+		if nums[r] == target {
+			*result = r
+			break
+		} else if nums[mid] > target {
+			r = mid
+		} else if nums[mid] < target {
+			l = mid
+		} else {
+			*result = mid
+			break
+		}
+		mid = (l + r) / 2
+	}
+	if nums[l] == target {
+		*result = l
+	} else if nums[r] == target {
+		*result = r
+	}
+}
+```
--- code.go
+++ code.go
@@ -8,44 +8,84 @@ type stack struct {
 }
 
 func search(nums []int, target int) int {
-	l, m, r := 0, len(nums)/2, len(nums)-1
-	for l != m || m != r {
-		if target > nums[l] && target < nums[m] {
-			return oneDirectionSearch(nums, target, l, m)
-		} else if target > nums[l] && target > nums[m] {
-			l, m, r = m, (m+r)/2, r
-		} else if target < nums[l] && target > nums[m] {
-			l, m, r = m, (m+r)/2, r
-		} else if target < nums[l] && target < nums[m] {
-			return oneDirectionSearch(nums, target, l, m)
-		} else if target == nums[l] {
-			return l
-		} else if target == nums[m] {
-			return m
-		} else if target == nums[r] {
-			return r
-		}
-	}
-	return -1
+	l, r := 0, len(nums)-1
+	result := -1
+	recurSearch(nums, target, l, r, &result)
+	return result
 }
 
-func oneDirectionSearch(nums []int, target int, l, r int) int {
+func recurSearch(nums []int, target int, l, r int, result *int) {
+	mid := (l + r) / 2
+	if mid == l {
+		if target == nums[l] {
+			*result = l
+		}
+		if target == nums[r] {
+			*result = r
+		}
+	} else {
+		if nums[mid] >= nums[l] {
+			if target >= nums[l] {
+				if nums[mid] >= target {
+					oneDirectionSearch(nums, target, l, mid, result)
+				} else {
+					recurSearch(nums, target, mid+1, r, result)
+				}
+			} else {
+				recurSearch(nums, target, mid+1, r, result)
+			}
+		} else {
+			if target >= nums[l] {
+				recurSearch(nums, target, l, mid, result)
+			} else {
+				if target <= nums[r] {
+					if target >= nums[mid] {
+						oneDirectionSearch(nums, target, mid, r, result)
+					} else {
+						recurSearch(nums, target, l, mid-1, result)
+					}
+				}
+			}
+		}
+	}
+}
 
+func oneDirectionSearch(nums []int, target int, l, r int, result *int) {
+	mid := (l + r) / 2
+	for mid != l {
+		if nums[r] == target {
+			*result = r
+			break
+		} else if nums[mid] > target {
+			r = mid
+		} else if nums[mid] < target {
+			l = mid
+		} else {
+			*result = mid
+			break
+		}
+		mid = (l + r) / 2
+	}
+	if nums[l] == target {
+		*result = l
+	} else if nums[r] == target {
+		*result = r
+	}
 }
 
 func main() {
 
-	// in := ")()())()()()(())))()()()()"
-	// in2 := "()(())"
-	in3 := ")()())"
-	in4 := ")(((((()())()()))()(()))("
-	in5 := ")(()(()(((())(((((()()))((((()()(()()())())())()))()()()())(())()()(((()))))()((()))(((())()((()()())((())))(())))())((()())()()((()((())))))((()(((((()((()))(()()(())))((()))()))())"
+	// in := []int{4, 5, 6, 7, 0, 1, 2, 3}
+	in2 := []int{1, 3, 5}
+	// in3 := ")()())"
+	// in4 := ")(((((()())()()))()(()))("
+	// in5 := ")(()(()(((())(((((()()))((((()()(()()())())())()))()()()())(())()()(((()))))()((()))(((())()((()()())((())))(())))())((()())()()((()((())))))((()(((((()((()))(()()(())))((()))()))())"
 	// fmt.Println(longestValidParentheses(in))
 
-	// fmt.Println(longestValidParentheses(in2))
-	fmt.Println(longestValidParentheses(in3))
-	fmt.Println(longestValidParentheses(in4))
-	fmt.Println(longestValidParentheses(in5))
+	// fmt.Println(search(in, 0))
+	fmt.Println(search(in2, 3))
+	// fmt.Println(longestValidParentheses(in4))
+	// fmt.Println(longestValidParentheses(in5))
 
 	// fmt.Println(r2)
 
