724cb06434911b1f07294e4299ddaf0f76edb830
ft9738962 <lemonade475@163.com>
2020-11-19 17:56:30 +0800

    201119 36

--- 36 Valid Sudoku.md
+++ 36 Valid Sudoku.md
@@ -48,3 +48,51 @@ Same as Example 1, except with the 5 in the top left corner being modified to 8.
 - board.length == 9
 - board[i].length == 9
 - board[i][j] is a digit or '.'.
+
+### 思路1：
+这题的思路主要是用怎么样最小的数据格式储存已有的数独信息，第一种方法是效率较低的切片+字典形式储存，需要使用根据循环的进度，每9个数字各使用一个字典确认数字是否重复
+
+```go
+// runtime 4ms 70.33% memory 3.6MB 44.72%
+func isValidSudoku(board [][]byte) bool {
+	var hlineCheck map[byte]bool
+	var vlineCheck map[byte]bool
+	blockCheck := [9]map[byte]bool{}
+	for a := 0; a < 9; a++ {
+		blockCheck[a] = map[byte]bool{}
+	}
+
+	for i := 0; i < 9; i++ {
+		hlineCheck = map[byte]bool{}
+		vlineCheck = map[byte]bool{}
+		for j := 0; j < 9; j++ {
+			if board[i][j] == 46 {
+			} else if hlineCheck[board[i][j]] == false {
+				hlineCheck[board[i][j]] = true
+			} else {
+				return false
+			}
+
+			if board[j][i] == 46 {
+			} else if vlineCheck[board[j][i]] == false {
+				vlineCheck[board[j][i]] = true
+			} else {
+				return false
+			}
+
+			if board[i][j] == 46 {
+			} else if blockCheck[(i/3)*3+j/3][board[i][j]] == false {
+				blockCheck[(i/3)*3+j/3][board[i][j]] = true
+			} else {
+				return false
+			}
+		}
+	}
+	return true
+}
+```
+
+### 思路2
+使用bit来储存，用9个9bit来表示横排，9个9bit表示竖排，9个9bit表示区块，这样只需要243个bit就能够包含数独的所有信息，对应的就是4个uint64数据
+
+```
--- code.go
+++ code.go
@@ -8,33 +8,29 @@ import "fmt"
 // }
 
 func isValidSudoku(board [][]byte) bool {
-	hlineCheck := map[int]bool{}
-	vlineCheck := map[int]bool{}
-	blockCheck := [9]map[int]bool{}
-
+	var pos = [4]uint64{0, 0, 0, 0}
+	var blockNo, blockPos, hPos, vPos int
 	for i := 0; i < 9; i++ {
 		for j := 0; j < 9; j++ {
-			if string(board[i][j]) == "." {
+			num := int(board[i][j] - 48)
+			if num+2 == 0 {
 				continue
-			} else if hlineCheck[int(board[i][j])] == false {
-				hlineCheck[int(board[i][j])] = true
-			} else {
-				return false
 			}
 
-			if string(board[j][i]) == "." {
-				continue
-			} else if vlineCheck[int(board[j][i])] == false {
-				vlineCheck[int(board[j][i])] = true
-			} else {
-				return false
-			}
+			blockNo = (i/3)*3 + j/3
+			blockPos = num + blockNo*9
+			vPos = blockPos + 81
+			hPos = vPos + 81
 
-			if blockCheck[i*3+j][int(board[i][j])] == false {
-				blockCheck[i*3+j][int(board[i][j])] = true
-			} else {
+			if pos[blockPos/64]>>(blockPos%64)&1 == 1 ||
+				pos[vPos/64]>>(vPos%64)&1 == 1 ||
+				pos[hPos/64]>>(hPos%64)&1 == 1 {
 				return false
 			}
+			pos[blockPos/64] += 0x01 << (blockPos % 64)
+			pos[vPos/64] += 0x01 << (vPos % 64)
+			pos[hPos/64] += 0x01 << (hPos % 64)
+
 		}
 	}
 	return true
@@ -43,17 +39,25 @@ func isValidSudoku(board [][]byte) bool {
 func main() {
 
 	in := [][]byte{
-		[]byte{"8", "3", ".", ".", "7", ".", ".", ".", "."},
+		{53, 51, 46, 46, 55, 46, 46, 46, 46},
+		{54, 46, 46, 49, 57, 53, 46, 46, 46},
+		{46, 57, 56, 46, 46, 46, 46, 54, 46},
+		{56, 46, 46, 46, 54, 46, 46, 46, 51},
+		{52, 46, 46, 56, 46, 51, 46, 46, 49},
+		{55, 46, 46, 46, 50, 46, 46, 46, 54},
+		{46, 54, 46, 46, 46, 46, 50, 56, 46},
+		{46, 46, 46, 52, 49, 57, 46, 46, 53},
+		{46, 46, 46, 46, 56, 46, 46, 55, 57},
 	}
-	in2 := []int{5, 7, 7, 8, 8, 10}
-	in3 := []int{}
+	// in2 := []int{5, 7, 7, 8, 8, 10}
+	// in3 := []int{}
 	// in4 := ")(((((()())()()))()(()))("
 	// in5 := ")(()(()(((())(((((()()))((((()()(()()())())())()))()()()())(())()()(((()))))()((()))(((())()((()()())((())))(())))())((()())()()((()((())))))((()(((((()((()))(()()(())))((()))()))())"
 	// fmt.Println(longestValidParentheses(in))
 
-	fmt.Println(searchRange(in, 1))
-	fmt.Println(searchRange(in2, 8))
-	fmt.Println(searchRange(in3, 0))
+	fmt.Println(isValidSudoku(in))
+	// fmt.Println(searchRange(in2, 8))
+	// fmt.Println(searchRange(in3, 0))
 	// fmt.Println(longestValidParentheses(in4))
 	// fmt.Println(longestValidParentheses(in5))
 
