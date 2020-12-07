9ccc7d66689b3a97e1492f081061a6aa460fc666
ft9738962 <lemonade475@163.com>
2020-12-07 17:26:24 +0800

    201207 39

--- code.go
+++ code.go
@@ -12,49 +12,46 @@ import (
 func combinationSum(candidates []int, target int) [][]int {
 	solutions := [][]int{}
 	recMap := make(map[int]bool)
-
+	recurTry(candidates, target, []int{}, &solutions, &recMap)
 	return solutions
 }
 
-func try(candidates []int, target int, solution []int, solutions *[][]int, recMap *map[int]bool) {
+func recurTry(candidates []int, target int, solution []int, solutions *[][]int, recMap *map[int]bool) {
 	for i := len(candidates) - 1; i >= 0; i-- {
 		newTarget := target - candidates[i]
-		if ok := recMap[newTarget] {
-			
-		}
+
 		if newTarget == 0 {
 			solution = append(solution, candidates[i])
 			*solutions = append(*solutions, solution)
-			break
-		} else if newTarget > 0 {
-			&recMap[newTarget] = 1
-			try(candidates, newTarget, append(solution, candidates[i]), solutions)
-		} else {
+			solution = []int{}
 			continue
+		} else if newTarget > 0 {
+			if ok := (*recMap)[newTarget]; ok == true {
+				continue
+			} else {
+				solution = append(solution, candidates[i])
+				recurTry(candidates[:i+1], newTarget, solution, solutions, recMap)
+				(*recMap)[newTarget] = true
+			}
+		} else {
+			if i == 0 {
+				solution = []int{}
+			}
+			break
 		}
 	}
 }
 
 func main() {
 
-	in := [][]byte{
-		{53, 51, 46, 46, 55, 46, 46, 46, 46},
-		{54, 46, 46, 49, 57, 53, 46, 46, 46},
-		{46, 57, 56, 46, 46, 46, 46, 54, 46},
-		{56, 46, 46, 46, 54, 46, 46, 46, 51},
-		{52, 46, 46, 56, 46, 51, 46, 46, 49},
-		{55, 46, 46, 46, 50, 46, 46, 46, 54},
-		{46, 54, 46, 46, 46, 46, 50, 56, 46},
-		{46, 46, 46, 52, 49, 57, 46, 46, 53},
-		{46, 46, 46, 46, 56, 46, 46, 55, 57},
-	}
+	in := []int{2, 3, 6, 7}
 	// in2 := []int{5, 7, 7, 8, 8, 10}
 	// in3 := []int{}
 	// in4 := ")(((((()())()()))()(()))("
 	// in5 := ")(()(()(((())(((((()()))((((()()(()()())())())()))()()()())(())()()(((()))))()((()))(((())()((()()())((())))(())))())((()())()()((()((())))))((()(((((()((()))(()()(())))((()))()))())"
 	// fmt.Println(longestValidParentheses(in))
 
-	fmt.Println(isValidSudoku(in))
+	fmt.Println(combinationSum(in, 7))
 	// fmt.Println(searchRange(in2, 8))
 	// fmt.Println(searchRange(in3, 0))
 	// fmt.Println(longestValidParentheses(in4))
